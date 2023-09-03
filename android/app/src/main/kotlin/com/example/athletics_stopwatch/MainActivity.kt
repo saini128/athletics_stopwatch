b1ea34ec76731f83be1168d2c3c3c15fe1171c13
Hushraj Singh <saini.hck@gmail.com>
2023-09-03 17:40:27 +0530

    ui complete

--- android/app/src/main/kotlin/com/example/athletics_stopwatch/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/athletics_stopwatch/MainActivity.kt
@@ -3,4 +3,5 @@ package com.example.athletics_stopwatch
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
+    
 }
--- lib/home.dart
+++ lib/home.dart
@@ -1,6 +1,6 @@
 import 'dart:async';
-
 import 'package:flutter/material.dart';
+
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -10,11 +10,12 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  int sec = 0, min = 0, hrs = 0;
-  String digSec = "00", digMin = "00", digHrs = "00";
+  int sec = 0, min = 0, hrs = 0, ms = 0;
+  String digSec = "00", digMin = "00", digHrs = "00", digMS = "00";
   Timer? timer;
   bool started = false;
   List laps = [];
+  List revLaps = [];
 
   void stop() {
     timer!.cancel();
@@ -25,14 +26,52 @@ class _HomeState extends State<Home> {
 
   void reset() {
     timer!.cancel();
+    laps = [];
     setState(() {
       sec = 0;
       min = 0;
       hrs = 0;
+      ms = 0;
       digHrs = "00";
       digMin = "00";
       digSec = "00";
+      digMS = "00";
       started = false;
+    });
+  }
+
+  void addLaps() {
+    String lap = "$digMin:$digSec:$digMS";
+    setState(() {
+      laps.add(lap);
+      revLaps = new List.from(laps.reversed);
+    });
+  }
+
+  void start() {
+    started = true;
+    timer = Timer.periodic(Duration(milliseconds: 20), (timer) {
+      int localMS = ms + 20;
+      int localSec = sec;
+      int localMin = min;
+
+      if (localMS > 980) {
+        if (localSec > 59) {
+          localMin++;
+          localSec = 0;
+        } else {
+          localSec++;
+          localMS = 0;
+        }
+      }
+      setState(() {
+        ms = localMS;
+        sec = localSec;
+        min = localMin;
+        digSec = (sec >= 10) ? "$sec" : "0$sec";
+        digMin = (min >= 10) ? "$min" : "0$min";
+        digMS = (ms >= 100) ? "$ms".substring(0, 2) : "0$ms".substring(0, 2);
+      });
     });
   }
 
@@ -61,18 +100,44 @@ class _HomeState extends State<Home> {
             ),
             Center(
               child: Text(
-                '00:00:00',
+                "$digMin:$digSec:$digMS",
                 style: TextStyle(
                   color: Colors.white,
-                  fontSize: 82,
+                  fontSize: 70,
                   fontWeight: FontWeight.bold,
                 ),
               ),
             ),
             Container(
-              height: 400,
+              height: 300,
               decoration: BoxDecoration(
                   color: Colors.blue, borderRadius: BorderRadius.circular(10)),
+              child: ListView.builder(
+                  itemCount: laps.length,
+                  itemBuilder: (context, index) {
+                    return Padding(
+                      padding: const EdgeInsets.all(10),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "Lap ${laps.length - index}",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 16,
+                            ),
+                          ),
+                          Text(
+                            "${revLaps[index]}",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 16,
+                            ),
+                          )
+                        ],
+                      ),
+                    );
+                  }),
             ),
             SizedBox(
               height: 10,
@@ -82,12 +147,14 @@ class _HomeState extends State<Home> {
               children: [
                 Expanded(
                   child: RawMaterialButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      (!started) ? start() : stop();
+                    },
                     shape: const StadiumBorder(
                       side: BorderSide(color: Colors.blue),
                     ),
                     child: Text(
-                      "Start",
+                      (!started) ? "Start" : "Stop",
                       style: TextStyle(color: Colors.white),
                     ),
                   ),
@@ -95,17 +162,22 @@ class _HomeState extends State<Home> {
                 SizedBox(
                   width: 8,
                 ),
-                IconButton(onPressed: () {}, icon: Icon(Icons.flag)),
+                IconButton(
+                    color: Colors.white,
+                    onPressed: () {},
+                    icon: Icon(Icons.flag)),
                 SizedBox(
                   width: 8,
                 ),
                 Expanded(
                   child: RawMaterialButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      (!started) ? reset() : addLaps();
+                    },
                     fillColor: Colors.blue,
                     shape: const StadiumBorder(),
                     child: Text(
-                      "Start",
+                      (!started) ? "Reset" : "Lap",
                       style: TextStyle(color: Colors.white),
                     ),
                   ),
